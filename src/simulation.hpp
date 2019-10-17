9e9dc4419a627c410c8202b6ebac71016d9f9cb3
Odagawa <xtctl.1234@gmail.com>
2019-10-17 12:30:23 +0900

    mesh計算の形の作成

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -16,13 +16,14 @@ namespace simulation
 {
     // xy -> fxy
     // u :ans
-    void simulate(eg::Matrix<std::vector<float>, eg::Dynamic, eg::Dynamic>& xys, eg::MatrixXcf& u, const std::function<float(float)> f,
+    template<typename FuncType> // 本当は関数の部分型にしたいがコンパイル時にチェックさせれば大丈夫
+    void simulate(eg::Matrix<std::vector<float>, eg::Dynamic, eg::Dynamic>& xys, eg::MatrixXcf& u, const FuncType f,
                     std::vector< float > range, const float r, const float k) {
         
         
-        const eg::MatrixXf fxys = xys.unaryExpr(f); // f(x, y)
+        const eg::MatrixXf fxys = calc_xy_mesh(range, f); // f(x, y)
 
-        u = xys.unaryExpr([fxys, range, k, r](std::vector<float> xy){ return integral(xy[0], xy[1], fxys, range, k, r);});
+        u = calc_xy_mesh(range, [fxys, range, k, r](std::vector<float> xy){ return integral(xy[0], xy[1], fxys, range, k, r);});
         
         u = u * std::exp(I*k*r) / r;
 
@@ -39,6 +40,11 @@ namespace simulation
             }
         }
         return sum;
+    }
+
+    template<typename T, typename FuncType>
+    static eg::Matrix<T, eg::Dynamic, eg::Dynamic> calc_xy_mesh(std::vector<float> range, FuncType f) {
+
     }
 
     
