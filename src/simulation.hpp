701d2539cf8c116e3d77fa8f05e3e8cdc1ef962a
Odagawa <xtctl.1234@gmail.com>
2019-10-17 12:03:47 +0900

    eigenの導入

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -1,30 +1,47 @@
+# define EIGEN_NO_DEBUG // コード内のassertを無効化．
+# define EIGEN_MPL2_ONLY // LGPLライセンスのコードを使わない．
+
 # include <vector>
 # include <complex>
 # include <functional>
+# include "Eigen/Dense"
 
-template<typename T> // float, double などを想定。変更可能にした
-using Vect2 = std::vector< std::vector<T> >;
+namespace eg = Eigen;
+constexpr std::complex<float> I(0, 1);
+
+// template<typename T> // float, double などを想定。変更可能にした
+// using MatrixX = eg::Matrix<T, eg::Dynamic, eg::Dynamic>;
+
 namespace simulation
 {
-    template<typename T>
-    void map_f(Vect2<T>& xy_array, const std::function<T(T)> f) {
-        // xy_arrayは別途メモリ確保せず、fを各成分に作用させる
-        for (auto& arr : xy_array) {
-            for (auto& value : arr) {
-                value = f(value);
-            }
-        }
-    }
-    template<typename T>
-    void simulate(const Vect2<T>& xy_array, Vect2< std::complex<T> >& gr, const std::function<T(T)> f,
-                const float mu_x, const float mu_y) {
-        // gr -> 求めるべきもの
-        // xy_arrayはfを各成分に作用させたもの
+    // xy -> fxy
+    // u :ans
+    void simulate(eg::Matrix<std::vector<float>, eg::Dynamic, eg::Dynamic>& xys, eg::MatrixXcf& u, const std::function<float(float)> f,
+                    std::vector< float > range, const float r, const float k) {
         
         
+        const eg::MatrixXf fxys = xys.unaryExpr(f); // f(x, y)
+
+        u = xys.unaryExpr([fxys, range, k, r](std::vector<float> xy){ return integral(xy[0], xy[1], fxys, range, k, r);});
+        
+        u = u * std::exp(I*k*r) / r;
+
         
 
     }
+    
+    static std::complex<float> integral(float x_p, float y_p, const eg::MatrixXf& fxy, std::vector< float > range, const float k, const float r) {
+        
+        std::complex<float> sum(0, 0);
+        for (const auto& x : range) {
+            for (const auto& y : range) {
+                sum += std::exp( I * k / r *(x*x_p + y*y_p) );
+            }
+        }
+        return sum;
+    }
+
+    
 } // namespace simulation
 
 
