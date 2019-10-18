7a825d47320476e0aaaf53146f81320798c4e050
Odagawa <xtctl.1234@gmail.com>
2019-10-19 00:26:03 +0900

    visualize

--- src/circle.cpp
+++ src/circle.cpp
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
     constexpr float wavelength_nm = 632.8; // nm
     constexpr float k = 2 * M_PI / (wavelength_nm * 1e-6);
     constexpr float r = 400; // スクリーンとの距離
-    constexpr float r0 = 10; // 円の大きさ
+    constexpr float r0 = 10; // 円の半径
     constexpr float screen_range = 50; // スクリーンの幅/2
     const eg::VectorXf range0 = eg::VectorXf::LinSpaced(100, -r0, r0);
     const eg::VectorXf range1 = eg::VectorXf::LinSpaced(100, -screen_range, screen_range);
