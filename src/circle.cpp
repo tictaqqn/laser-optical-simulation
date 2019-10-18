2eb26dafe4d5eef3d856faa30e30242d10678eb6
Odagawa <xtctl.1234@gmail.com>
2019-10-18 23:42:41 +0900

    計算した

--- src/circle.cpp
+++ src/circle.cpp
@@ -7,6 +7,7 @@
 using namespace simulation;
 namespace eg = Eigen;
 
+// コンパイルのオプションは-O3 -funroll-loops
 int main(int argc, char const *argv[])
 {
     /* 波長以外すべてmm単位 */
@@ -18,8 +19,13 @@ int main(int argc, char const *argv[])
     const eg::VectorXf range0 = eg::VectorXf::LinSpaced(100, -r0, r0);
     const eg::VectorXf range1 = eg::VectorXf::LinSpaced(100, -screen_range, screen_range);
 
+    std::cout << "wavelength: " << wavelength_nm << " R: " << r << " r0: " << r0 << " LinSpaced num:" 
+        << "100 100" << std::endl;
+
     eg::MatrixXf grr;
     simulate(grr, circle, range0, range1, r, k);
+
+    std::cout << grr << std::endl;
 
     return 0;
 }
