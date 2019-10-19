fc0fe119fabd9a6dca96b4f2e7e2cb3f65c90183
Odagawa <xtctl.1234@gmail.com>
2019-10-19 11:18:09 +0900

    並行処理実装完了

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -24,7 +24,7 @@ namespace simulation
             }
         }
     }
-    static std::complex<float> integral(float x_p, float y_p, const eg::MatrixXi& fxy, const eg::VectorXf& range_flt, const float k, const float r) {
+    static std::complex<float> integral(const float x_p, const float y_p, const eg::MatrixXi& fxy, const eg::VectorXf& range_flt, const float k, const float r) {
         
         std::complex<float> sum(0, 0);
         constexpr int n_th = 8;
@@ -33,11 +33,13 @@ namespace simulation
         {
             # pragma omp for reduction(+:sum)
             for(int th=0; th<n_th; ++th) {
-                for (int i=0; i<range_flt.size()/n_th; ++i) {
-                    if(k==n_th-1 && i+range_flt.size()/n_th*th>=range_flt.size()) break;
+                for (int i=0; i < (th!=n_th-1 ? range_flt.size()/n_th : range_flt.size()/n_th * 2); ++i) {
+                    int ii = i+range_flt.size()/n_th*th;
+                    if(th==n_th-1 && ii>=range_flt.size()-1) break;
+                    // std::cout <<th <<" "<< i << " " << ii << std::endl;
                     for (int j=0; j<range_flt.size(); ++j) {
-                        if (fxy(i+range_flt.size()/n_th*th, j) == 1)
-                            sum += std::exp( I * k / r *(range_flt(i+range_flt.size()/n_th*th)*x_p + range_flt(j)*y_p) );
+                        if (fxy(ii, j) == 1)
+                            sum += std::exp( I * k / r *(range_flt(ii)*x_p + range_flt(j)*y_p) );
                     }
                 }
             }
