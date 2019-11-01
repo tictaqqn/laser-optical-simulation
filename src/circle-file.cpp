10e7bde0af967317a2a871dca0b4d2db67f8c070
Odagawa <xtctl.1234@gmail.com>
2019-11-01 09:44:54 +0900

    circle-file init

--- src/circle-file.cpp
+++ src/circle-file.cpp
@@ -18,18 +18,24 @@ int main(int argc, char const *argv[])
     /* 波長以外すべてmm単位 */
     constexpr float wavelength_nm = 632.8; // nm
     constexpr float k = 2 * M_PI / (wavelength_nm * 1e-6);
-    constexpr float r = 400; // スクリーンとの距離
+    // constexpr float r = 400; // スクリーンとの距離
     constexpr float r0 = 10; // 円の半径
     constexpr float screen_range = 0.1; // スクリーンの幅/2
     constexpr int dense_flt = 100;
     constexpr int dense_sc = 1000;
     const eg::VectorXf range_flt = eg::VectorXf::LinSpaced(dense_flt, -r0, r0);
     const eg::VectorXf range_sc = eg::VectorXf::LinSpaced(dense_sc, -screen_range, screen_range);
+    const eg::VectorXf rs = eg::VectorXf::LinSpaced(20, 300, 500);
 
-    std::string file_path;
+    std::string file_path; // timestamp
     std::ofstream writing_file(file_path, std::ios::out);
 
-    writing_file << "" << std::endl;
+    for (int i=0; i<rs.size(); ++i) {
+        eg::MatrixXf grr;
+        simulate(grr, circle, range_flt, range_sc, rs(i), k, r0);
+        writing_file << grr << std::endl;
+    }
+
 
     return 0;
 }
