5129cff6f775bafce6bf03ce11ddc0aa70b1d86f
Odagawa <xtctl.1234@gmail.com>
2019-11-18 16:39:31 +0900

    file names

--- src/circle-file.cpp
+++ src/circle-file.cpp
@@ -9,10 +9,12 @@
 // # include <bits/stdc++.h> // for M_PI
 # include "Eigen/Dense"
 # include "simulation.hpp"
+# include "get_time_stamp.hpp"
 using namespace simulation;
 namespace eg = Eigen;
 
 // コンパイルのオプションは-O3 -funroll-loops -fopenmp
+
 int main(int argc, char const *argv[])
 {
     /* 波長以外すべてmm単位 */
@@ -27,8 +29,15 @@ int main(int argc, char const *argv[])
     const eg::VectorXf range_sc = eg::VectorXf::LinSpaced(dense_sc, -screen_range, screen_range);
     const eg::VectorXf rs = eg::VectorXf::LinSpaced(20, 300, 500);
 
-    std::string file_path; // timestamp
+    std::string file_path("results/circle/");
+    std::stringstream ss;
+    ss << "wavelength: " << wavelength_nm << " R: " << r << " r0: " << r0 << " dense_flt: " << dense_flt 
+        << " dense_sc: " << dense_sc;
+    file_path += ss.str();
+    file_path += getTimeStamp(); // timestamp
     std::ofstream writing_file(file_path, std::ios::out);
+
+    writing_file << ss.str() << std::endl;
 
     for (int i=0; i<rs.size(); ++i) {
         eg::MatrixXf grr;
@@ -39,3 +48,5 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
+
+
---
+++ src/get_time_stamp.hpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+
+std::string getTimeStamp()
+{
+    auto now = std::chrono::system_clock::now();
+    auto now_c = std::chrono::system_clock::to_time_t(now);
+    std::stringstream ss;
+    ss << std::put_time(localtime(&now_c), "%Y%m%d_%H%M%S");
+    return ss.str();
+}
+
+//----------出力例----------
+//20171231_145030
+
+//2017年12月31日 14時50分30秒のこと
