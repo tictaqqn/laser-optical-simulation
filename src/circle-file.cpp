0eaf1f86f6d1051d13c7df515a611d1c5896b79c
Odagawa <xtctl.1234@gmail.com>
2019-11-19 10:39:49 +0900

    calc 20

--- src/circle-file.cpp
+++ src/circle-file.cpp
@@ -23,27 +23,33 @@ int main(int argc, char const *argv[])
     // constexpr float r = 400; // スクリーンとの距離
     constexpr float r0 = 10; // 円の半径
     constexpr float screen_range = 0.1; // スクリーンの幅/2
-    constexpr int dense_flt = 100;
-    constexpr int dense_sc = 1000;
-    const eg::VectorXf range_flt = eg::VectorXf::LinSpaced(dense_flt, -r0, r0);
-    const eg::VectorXf range_sc = eg::VectorXf::LinSpaced(dense_sc, -screen_range, screen_range);
-    const eg::VectorXf rs = eg::VectorXf::LinSpaced(20, 300, 500);
+    constexpr int n_flt = 100;
+    constexpr int n_sc = 1000;
+    constexpr int n_R = 20;
+    constexpr float R_min = 300;
+    constexpr float R_max = 500;
+    const eg::VectorXf range_flt = eg::VectorXf::LinSpaced(n_flt, -r0, r0);
+    const eg::VectorXf range_sc = eg::VectorXf::LinSpaced(n_sc, -screen_range, screen_range);
+    const eg::VectorXf rs = eg::VectorXf::LinSpaced(n_R, R_min, R_max);
 
-    std::string file_path("results/circle/");
+    std::string file_path("../results/circle/");
     std::stringstream ss;
-    ss << "wavelength: " << wavelength_nm << " R: " << r << " r0: " << r0 << " dense_flt: " << dense_flt 
-        << " dense_sc: " << dense_sc;
+    ss << "wavelength: " << wavelength_nm << " r0: " << r0 << " n_flt: " << n_flt 
+        << " n_sc: " << n_sc << " n_R: " << n_R << " R_min: " << R_min 
+        << " R_max: " << R_max;
     file_path += ss.str();
+    file_path += "__";
     file_path += getTimeStamp(); // timestamp
     file_path += ".txt";
     std::ofstream writing_file(file_path, std::ios::out);
 
-    writing_file << ss.str() << std::endl;
+    // writing_file << ss.str() << std::endl;
 
     for (int i=0; i<rs.size(); ++i) {
         eg::MatrixXf grr;
         simulate(grr, circle, range_flt, range_sc, rs(i), k, r0);
         writing_file << grr << std::endl;
+        std::cout << i << std::endl;
     }
 
 
